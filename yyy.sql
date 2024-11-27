40d9c86d836e9c7694ca0e7d675facafffcbdeb5
VAMSHIKRISHNA009 <VAMSHI31027@GMAIL.COM>
2024-11-27 23:41:28 +0530

    Add files via upload

---
+++ yyy.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM testdatabase.twitter
+LIMIT 0, 1000
+
+-- Date: 2024-11-27 22:02
+*/
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (0,'GTN UK','Providing Entertainment & Travel to Commercial Radio. Reaching 28.9M weekly listeners. Winners of The Arqiva National Sales Team of the Year 2010, 2011 & 2016','136',456,'London,England');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (1,'WhatsApp','push,push','          5.5M',4,'Calfornia');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (2,'A&A Customs Broker','customs broker','44',124,'Florida,USA');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (3,'A&A Customs Brokers','A & A Freight  Warehousing  Customs Brokerage  Helping people ship across borders.','700',3889,'Worldwide');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (4,'A&A Window Products','A commercial glass and glazing company serving the window industry in New England since 1954','82',88,'Malden,MA');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (5,'A&B Kia','A&B Kia is a Kia dealer in Benwood, WV. Stay connected to exceed expectations. Build strong relationships. Drive the best with us.','298',333,'Benwood,West Virginia');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (6,'A&B Home','Industry leader in wholesale home decor, furniture, and garden, since 1993.','367',178,'Rancho Cucamonga,CA');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (7,'A&B Reprographics','From large format black & white prints to the most sophisticated of digital color printing jobs, A & B can get the job done right.','134',123,'Bentonville,AR');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (8,'X','what\'s happening?!','        68.4M',0,'everywhere');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (9,'A&C CHRISTOFI LTD','A & C CHRISTOFI LTD is a fast growing professional services company based in Limassol, Cyprus. Our main Concern is your Business.','93',291,'Cyprus');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (10,'A&E Clothing','Used clothing Distributor','17',0,'Carteret');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (11,'X','what\'s happening?!','        68.4M',0,'what\'s happening?!');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (12,'A&E Technologies','none','34',1,'Boulder,CO USA');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (13,'Wix','Create, manage and grow your business online with Wix. It\'s more than just a website builder, it\'s how your vision comes to life. For support: @WixHelp.','          414K',3726,'We\'re everywhere');
+INSERT INTO `` (`MyUnknownColumn`,`profile name`,`profile bio`,`followerscount`,`following count`,`location`) VALUES (14,'A& Insurance LLC','A & G Insurance prides itself on being your local, full service, Independent Insurance Agency.','100',257,'Connecticut');
